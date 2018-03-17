83dbc2a28df1fcd9a80253d31030985e80c2d4c2
Saroja Ammula <saroja.ammula@gmail.com>
2018-03-17 12:29:55 -0700

    first commit

---
+++ textpredictor/src/main/java/text/predictor/TextpredictorApplication.java
@@ -0,0 +1,12 @@
+package text.predictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextpredictorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextpredictorApplication.class, args);
+	}
+}
